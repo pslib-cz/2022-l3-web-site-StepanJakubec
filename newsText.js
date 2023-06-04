4ac6bda5b8844b574fff0403a9f08a91b489ed25
StepanJakubec <107682109+StepanJakubec@users.noreply.github.com>
2023-06-05 01:33:44 +0200

    Add files via upload

---
+++ newsText.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const showButtons = document.querySelectorAll("#news-showText");
+    for (const button of showButtons) {
+        button.addEventListener("click", (event) => {
+            const div = event.target.parentNode;
+            const hiddenTextFinal = div.querySelector(".news_main_item__hiddenText");
+            hiddenTextFinal.classList.toggle("news-textActive");
+            if (button.textContent === "More") {
+                button.textContent = "Hide";
+              } else {
+                button.textContent = "More";
+              }
+        });
+    };
+});
