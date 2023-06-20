b56e9f5fe52105458bd490b1b5769cb8a4433e9b
StepanJakubec <107682109+StepanJakubec@users.noreply.github.com>
2023-06-20 21:55:05 +0200

    Delete hamburger.js

--- hamburger.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const hamburger = document.getElementById("hamburger");
-    hamburger.addEventListener("click", () => {
-        hamburger.classList.toggle("hamburgerMenu-active");
-        document.querySelector(".navbar__menu").classList.toggle("menu-active");
-    });
-});
